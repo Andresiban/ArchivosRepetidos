47731073882167ed1b65710b94068a972520390d
Andresiban <andres.ibanez@educa.madrid.org>
2021-11-04 21:11:36 +0100

    Archivos repetidos

---
+++ Ejercicio1.java
@@ -0,0 +1,11 @@
+package elementosBasicos;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Andrés!");
+        System.out.println("adios");
+    }
+
+
+}
